e8782a2ac9f549e21dc5be1731db5966698fc5b3
Emmanuel <alzadidine33@gmail.com>
2025-07-12 04:55:58 +0000

    A1601: Initial support Changes: - Renamed to A1601 - Imported blobs from F1s
    Signed-off-by: Emmanuel <alzadidine33@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,16 +1,22 @@
-LOCAL_PATH := device/oppo/A37
+#
+# Copyright (C) 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# Platform
-TARGET_BOARD_PLATFORM := mt6750
+LOCAL_PATH := device/oppo/A1601
 
-TARGET_IS_64_BIT := true
-TARGET_BOARD_SUFFIX := _64
-TARGET_USES_64_BIT_BINDER := true
-
-# Bootloader
-TARGET_NO_BOOTLOADER := true
-TARGET_BOOTLOADER_BOARD_NAME := mt6750
-
+# Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
@@ -21,42 +27,66 @@ TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := generic
-#TARGET_2ND_CPU_VARIANT := cortex-a53
+TARGET_2ND_CPU_VARIANT := cortex-a53
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6750
+TARGET_BOARD_SUFFIX := _64
+TARGET_IS_64_BIT := true
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := full_oppo6750_15331
+TARGET_NO_BOOTLOADER := true
 
 # Kernel
-BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 elevator=deadline
-BOARD_KERNEL_BASE := 0x40078000
-TARGET_KERNEL_ARCH := arm64
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS := --board 1466056865 --kernel_offset 0x00008000 --ramdisk_offset 0x04f88000 --tags_offset 0x03f88000
-TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/kernel
+BOARD_KERNEL_BASE := 0x40078000
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_RAMDISK_OFFSET := 0x04f88000
+BOARD_KERNEL_TAGS_OFFSET := 0x03f88000
+
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
+BOARD_KERNEL_IMAGE_NAME := kernel
+
+BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+
+# Encryption
+#TARGET_HW_DISK_ENCRYPTION := true
 
 # Partitions informations
-BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 22020096
-BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
+BOARD_BOOTIMAGE_PARTITION_SIZE := 9572352
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3221225472
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 27476852224 # (27476868608-16384)
+BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
 
-# Partitions types
+# Recovery
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_HAS_NO_SELECT_BUTTON := true
 TARGET_USERIMAGES_USE_EXT4 := true
 
-# TWRP
+# TWRP Configuration
 TW_THEME := portrait_hdpi
-TW_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
+TW_EXTRA_LANGUAGES := false
+TW_DEFAULT_BRIGHTNESS := 700
+TW_MAX_BRIGHTNESS := 2047
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_CUSTOM_CPU_TEMP_PATH := "/sys/devices/virtual/thermal/thermal_zone1/temp"
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_NO_REBOOT_BOOTLOADER := true
-TW_NO_HAPTICS := true
-TW_EXTRA_LANGUAGES := true
-TW_BUILD_ZH_CN_SUPPORT := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery/root/etc/twrp.fstab
+BOARD_SUPPRESS_SECURE_ERASE := true
+RECOVERY_SDCARD_ON_DATA := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+
+# Disable Mouse Cursor
+TW_INPUT_BLACKLIST := "hbtp_vm"
 
 # TWRP includes/excludes
 TW_EXCLUDE_SUPERSU := true
+TW_EXCLUDE_TWRPAPP := true
 TW_INCLUDE_NTFS_3G := true
-
-# Recovery
-TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery/recovery.fstab
-RECOVERY_SDCARD_ON_DATA := true
-BOARD_HAS_NO_SELECT_BUTTON := true
-BOARD_HAS_LARGE_FILESYSTEM := true
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun/file
+TW_INCLUDE_CRYPTO := false
---
+++ README.md
@@ -0,0 +1,22 @@
+TeamWin Recovery Project
+
+Device configuration for OPPO A59/F1s  (_A1601_)
+=====================================================
+
+Basic   | Spec Sheet
+-------:|:-------------------------
+CPU     | Octa-core 1.5 GHz Cortex-A53
+Chipset | MediaTek Helio P10 MT6750/55
+GPU     | Mali-T860MP2
+Memory  | 3/4 GB
+Shipped Android Version | Android 5.1.1 with ColorOS 3
+Storage | 32/64 GB
+MicroSD | Up to 256 GB (Hybrid)
+Battery | 3075 mAh (non-removable)
+Dimensions | 154.5 x 76 x 7.4 mm
+Display | 720 x 1280 pixels, 5.5" IPS
+Rear Camera  | 13.0 MP, LED flash
+Front Camera | 16.0 MP
+Release Date | August 2016
+
+![OPPO F1s](https://fdn2.gsmarena.com/vv/pics/oppo/oppo-f1s-1.jpg "OPPO F1s")
